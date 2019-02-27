a386c6b7d202626bb4b27995a7eb5f03ed1cfbf5
doandung2701 <doandung2701@gmail.com>
2019-02-27 11:27:18 +0700

    login using facebook,google, github

--- back-end/musicapp/src/main/java/com/hust/musicapp/musicapp/MusicappApplication.java
+++ back-end/musicapp/src/main/java/com/hust/musicapp/musicapp/MusicappApplication.java
@@ -1,9 +1,12 @@
 package com.hust.musicapp.musicapp;
 
+import com.hust.musicapp.musicapp.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class MusicappApplication {
 
     public static void main(String[] args) {
---
+++ back-end/musicapp/src/main/java/com/hust/musicapp/musicapp/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.hust.musicapp.musicapp.controller;
+
+import com.hust.musicapp.musicapp.exception.ResourceNotFoundException;
+import com.hust.musicapp.musicapp.model.User;
+import com.hust.musicapp.musicapp.repository.UserRepository;
+import com.hust.musicapp.musicapp.security.CurrentUser;
+import com.hust.musicapp.musicapp.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
