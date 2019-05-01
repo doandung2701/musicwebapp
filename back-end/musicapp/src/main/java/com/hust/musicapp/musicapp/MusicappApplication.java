a0a8b40db9a1dafdcf03804fe753adc6f3629c9f
doandung2701 <doandung2701@gmail.com>
2019-05-01 21:38:28 +0700

    create api upload download

--- back-end/musicapp/src/main/java/com/hust/musicapp/musicapp/MusicappApplication.java
+++ back-end/musicapp/src/main/java/com/hust/musicapp/musicapp/MusicappApplication.java
@@ -1,12 +1,13 @@
 package com.hust.musicapp.musicapp;
 
 import com.hust.musicapp.musicapp.config.AppProperties;
+import com.hust.musicapp.musicapp.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
+@EnableConfigurationProperties({AppProperties.class, FileStorageProperties.class})
 public class MusicappApplication {
 
     public static void main(String[] args) {
